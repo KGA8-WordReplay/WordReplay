b084cf65753d8ea700c7859f2ec1af0f29e14151
SIRO\gmlwo <gmlwo990518@naver.com>
2025-02-12 17:15:56 +0900

    [FEAT] : OnValueChanged를 감지해서 자동완성 하는 방식은 되는데, 살짝 느리다

--- Assets/3.Code/HEEJAETest/HEEJAEGameManager.cs
+++ Assets/3.Code/HEEJAETest/HEEJAEGameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using Unity.VisualScripting.FullSerializer;
@@ -11,6 +12,7 @@ public class HEEJAEGameManager : MonoBehaviour
     [SerializeField] private TMP_InputField input;
     [SerializeField] private TextMeshProUGUI word;
     [SerializeField] private TextMeshProUGUI mean;
+    [SerializeField] private TextMeshProUGUI suggestionText;
     [SerializeField] private Button confirmButton;
     [SerializeField] private string textFileName;
     [SerializeField] private string wordName;
@@ -27,6 +29,8 @@ public class HEEJAEGameManager : MonoBehaviour
     private void Awake()
     {
         confirmButton.onClick.AddListener(OnClickConfirmButton);
+        input.onValueChanged.AddListener(OnInputChanged); //값이 바뀔때마다 검사
+        input.onSubmit.AddListener(OnSubmit); //값이 바뀔때마다 검사
         _everyWord = HEEJAECSVReader.Read(textFileName);
     }
 
@@ -54,6 +58,19 @@ public class HEEJAEGameManager : MonoBehaviour
         {
             confirmButton.onClick.Invoke();
         }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            OnSubmit(input.text);
+        }
+        //if (!string.IsNullOrEmpty(Input.compositionString))
+        //{
+        //    Debug.Log($"조합 중 글자: {Input.compositionString}");
+        //}
+        //else
+        //{
+        //    Debug.Log($"완성된 텍스트: {input.text}");
+        //}
     }
 
     private void Init()
@@ -92,6 +109,7 @@ public class HEEJAEGameManager : MonoBehaviour
         input.text = "";
         input.ActivateInputField();
     }
+
     private bool IsInputWordInList(string inputText)
     {
         foreach (var word in _wordList)
@@ -139,8 +157,39 @@ public class HEEJAEGameManager : MonoBehaviour
         }
     }
 
-    private void StartWordChain()
+    //input안에 값이 바뀔때 마다 실행
+    private void OnInputChanged(string input)
     {
+        //버퍼에 아무것도 없으면
+        if (string.IsNullOrEmpty(input))
+        {
+            //비우기
+            suggestionText.text = "";
+            return;
+        }
 
+        //뭔가 있다면 그 단어로 시작하는 단어 찾기
+        string matchWord = _wordList.FirstOrDefault(word => word.StartsWith(input));
+
+        //매치되는 단어가 있으면
+        if (!string.IsNullOrEmpty(matchWord))
+        {
+            suggestionText.text = matchWord;
+        }
+        //없으면
+        else
+        {
+            suggestionText.text = "";
+        }
+    }
+
+    private void OnSubmit(string input)
+    {
+        if (!string.IsNullOrEmpty(suggestionText.text))
+        {
+            this.input.text = suggestionText.text;
+            //버퍼 뒤로 이동
+            this.input.MoveTextEnd(false);
+        }
     }
 }
