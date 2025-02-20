bba090702f188acb1cef359780c1b5a542751b45
l97038789 <l97038789@gmail.com>
2025-02-20 13:14:18 +0900

    [WIP] : 스테이지 페이지 기능 구현 중

--- Assets/3.Code/UI/StagePage.cs
+++ Assets/3.Code/UI/StagePage.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StagePage : Page
 {
+    private Button _backButton;
 
+    private void Awake()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>();
+
+        _backButton = buttons[0];
+
+        _backButton.onClick.AddListener(BackButtonClick);
+    }
+
+    private void BackButtonClick()
+    {
+        PageManager.Instance.OpenPage<TitlePage>();
+    }
 }
