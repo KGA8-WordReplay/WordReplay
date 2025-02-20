e2cd4a14cf75f82bfd9025e326335f7e3a2a1b41
l97038789 <l97038789@gmail.com>
2025-02-20 15:38:20 +0900

    [WIP] : 스테이지별 점수 구현 중

---
+++ Assets/3.Code/Stage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Stage : MonoBehaviour
+{
+    public int achieveScore;
+    public bool isLock = false;
+
+    private Button _stageButton;
+    private GameObject _lockGO;
+
+    private void Awake()
+    {
+        _stageButton = GetComponent<Button>();
+        _lockGO = transform.Find("Lock").gameObject;
+
+        _stageButton.onClick.AddListener(StageButtonClick);
+    }
+
+    private void Start()
+    {
+        LockStage();
+    }
+
+    private void StageButtonClick()
+    {
+        SceneManager.LoadScene("WordReplayScene");
+    }
+
+    public void LockStage()
+    {
+        if (isLock)
+        {
+            _stageButton.interactable = false;
+            _lockGO.SetActive(true);
+        }
+        else
+        {
+            _stageButton.interactable = true;
+            _lockGO.SetActive(false);
+        }
+    }
+}
