d8be7f66b63da9d4b75ed1f2ef6a88124663f6d7
l97038789 <l97038789@gmail.com>
2025-02-24 16:14:31 +0900

    [STYLE] : UserData 초기화 할 수 있도록 코드 추가

--- Assets/3.Code/Manager/UserDataManager.cs
+++ Assets/3.Code/Manager/UserDataManager.cs
@@ -5,63 +5,74 @@ using UnityEngine;
 
 public class UserDataManager : Singleton<UserDataManager>
 {
-	private UserData _userData;
+    [Header("UserData를 날리고 싶으시면 true를 하고 게임을 시작하세요.")]
+    public bool isInit;
 
-	protected override void Awake()
-	{
-		base.Awake();
-		if (Instance == this)
-		{
-			Load();
-		}
-	}
+    private UserData _userData;
 
-	public void Save(int gold)
-	{
-		_userData.AddGold(gold);
-		SaveUserData();
-	}
-	public void Save(string collectionName)
-	{
-		_userData.AddCollectionName(collectionName);
-		SaveUserData();
-	}
-	public void SaveStageUnlock(string stage)
-	{
-		_userData.AddStageUnlock(stage);
-		SaveUserData();
-	}
+    protected override void Awake()
+    {
+        base.Awake();
+        if (Instance == this)
+        {
+            if (isInit) Init();
+            Load();
+        }
+    }
 
-	public bool IsStageLock(string stageName)
-	{
-		return !_userData.IsStageUnlock(stageName);
-	}
+    public void Init()
+    {
+        PlayerPrefs.DeleteAll();
+    }
 
-	private void SaveUserData()
-	{
-		string json = JsonConvert.SerializeObject(_userData);
-		PlayerPrefs.SetString("userData", json);
-		PlayerPrefs.Save();
-		print("UserData가 저장됨: " + json);
-	}
+    public void Save(int gold)
+    {
+        _userData.AddGold(gold);
+        SaveUserData();
+    }
+    public void Save(string collectionName)
+    {
+        _userData.AddCollectionName(collectionName);
+        SaveUserData();
+    }
+    public void SaveStageUnlock(string stage)
+    {
+        _userData.AddStageUnlock(stage);
+        SaveUserData();
+    }
 
-	private void Load()
-	{
-		//UserData가 있을 때
-		if (PlayerPrefs.HasKey("userData"))
-		{
-			string json = PlayerPrefs.GetString("userData");
-			_userData = JsonConvert.DeserializeObject<UserData>(json);
-			print("저장된 UserData가 불러와짐.");
-			_userData.Test(_userData);
-		}
-		//UserData가 없다면 생성해서 할당
-		else
-		{
-			_userData = new UserData();
-			print("새로운 UserData가 불러와짐.");
-		}
 
-		SaveStageUnlock("stage1");
-	}
+
+    public bool IsStageLock(string stageName)
+    {
+        return !_userData.IsStageUnlock(stageName);
+    }
+
+    private void SaveUserData()
+    {
+        string json = JsonConvert.SerializeObject(_userData);
+        PlayerPrefs.SetString("userData", json);
+        PlayerPrefs.Save();
+        print("UserData가 저장됨: " + json);
+    }
+
+    private void Load()
+    {
+        //UserData가 있을 때
+        if (PlayerPrefs.HasKey("userData"))
+        {
+            string json = PlayerPrefs.GetString("userData");
+            _userData = JsonConvert.DeserializeObject<UserData>(json);
+            print("저장된 UserData가 불러와짐.");
+            _userData.Test(_userData);
+        }
+        //UserData가 없다면 생성해서 할당
+        else
+        {
+            _userData = new UserData();
+            print("새로운 UserData가 불러와짐.");
+        }
+
+        SaveStageUnlock("stage1");
+    }
 }
--- Assets/3.Code/UserData.cs
+++ Assets/3.Code/UserData.cs
@@ -6,57 +6,61 @@ using UnityEngine;
 [System.Serializable]
 public class UserData
 {
-	[JsonProperty] private int gold;
-	[JsonProperty] private List<string> collectionNames;
-	[JsonProperty] private List<string> stagesUnlock;
+    [JsonProperty] private int gold;
+    [JsonProperty] private List<string> collectionNames;
+    [JsonProperty] private List<string> stagesUnlock;
 
-	public UserData()
-	{
-		gold = 0;
-		collectionNames = new List<string>();
-		stagesUnlock = new List<string>();
-	}
+    public UserData()
+    {
+        gold = 0;
+        collectionNames = new List<string>();
+        stagesUnlock = new List<string>();
+    }
 
-	//Add
-	public void AddGold(int value) { gold += value; }
+    //Add
+    public void AddGold(int value) { gold += value; }
 
-	public void AddCollectionName(string collectionName) { collectionNames.Add(collectionName); }
+    public void AddCollectionName(string collectionName)
+    {
+        if (!collectionNames.Contains(collectionName))
+            collectionNames.Add(collectionName);
+    }
 
-	public void AddStageUnlock(string stageName)
-	{
-		if (!stagesUnlock.Contains(stageName))
-		{
-			stagesUnlock.Add(stageName);
-		}
-	}
+    public void AddStageUnlock(string stageName)
+    {
+        if (!stagesUnlock.Contains(stageName))
+        {
+            stagesUnlock.Add(stageName);
+        }
+    }
 
-	//Sub
-	public void SubGold(int value) { gold -= value; }
+    //Sub
+    public void SubGold(int value) { gold -= value; }
 
-	//Get
-	public int GetGold() { return gold; }
+    //Get
+    public int GetGold() { return gold; }
 
-	public List<string> GetCollectionNames() { return collectionNames; }
+    public List<string> GetCollectionNames() { return collectionNames; }
 
 
-	//bool
-	public bool IsStageUnlock(string stageName)
-	{
-		return stagesUnlock.Contains(stageName);
-	}
+    //bool
+    public bool IsStageUnlock(string stageName)
+    {
+        return stagesUnlock.Contains(stageName);
+    }
 
-	public void Test(UserData userData)
-	{
-		Debug.Log($"골드량: {userData.gold}");
-		Debug.Log("갖고 있는 사전");
-		foreach (string collectionName in userData.collectionNames)
-		{
-			Debug.Log($"이름: {collectionName}");
-		}
-		Debug.Log("클리어한 스테이지");
-		foreach (string stageName in userData.stagesUnlock)
-		{
-			Debug.Log($"스테이지: {stageName}");
-		}
-	}
+    public void Test(UserData userData)
+    {
+        Debug.Log($"골드량: {userData.gold}");
+        Debug.Log("갖고 있는 사전");
+        foreach (string collectionName in userData.collectionNames)
+        {
+            Debug.Log($"이름: {collectionName}");
+        }
+        Debug.Log("클리어한 스테이지");
+        foreach (string stageName in userData.stagesUnlock)
+        {
+            Debug.Log($"스테이지: {stageName}");
+        }
+    }
 }
