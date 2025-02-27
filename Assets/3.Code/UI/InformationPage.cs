14c4911c334aaf13743452e61fdf4bca4e71e24a
l97038789 <l97038789@gmail.com>
2025-02-27 10:19:39 +0900

    [WIP] : 도움말 기능 구현 중

--- Assets/3.Code/UI/InformationPage.cs
+++ Assets/3.Code/UI/InformationPage.cs
@@ -10,29 +10,48 @@ public class InformationPage : Page
     [SerializeField] private Button _nextButton;
 
     public List<GameObject> informations = new List<GameObject>();
-    private Stack<GameObject> informationStack = new Stack<GameObject>();
+    private int currentIndex = 0;
 
     private void Start()
     {
         _prevButton.onClick.AddListener(PrevButtonClick);
         _nextButton.onClick.AddListener(NextButtonClick);
 
+        UpdatePage();
+    }
+
+    private void PrevButtonClick()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            UpdatePage();
+        }
+    }
+
+    private void NextButtonClick()
+    {
+        if (currentIndex < informations.Count - 1)
+        {
+            currentIndex++;
+            UpdatePage();
+        }
+    }
+
+    private void UpdatePage()
+    {
+        // 모든 오브젝트 비활성화
         foreach (GameObject obj in informations)
         {
             obj.SetActive(false);
         }
 
-        informations[0].SetActive(true);
-    }
+        // 현재 인덱스의 오브젝트 활성화
+        informations[currentIndex].SetActive(true);
 
-    private void PrevButtonClick()
-    {
-
-    }
-
-    private void NextButtonClick()
-    {
-
+        // 버튼 활성/비활성 설정
+        _prevButton.interactable = currentIndex > 0;
+        _nextButton.interactable = currentIndex < informations.Count - 1;
     }
 
     private void OnDestroy()
